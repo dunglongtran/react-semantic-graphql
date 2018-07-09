124ff4c606c9b717f811c8c996b017a6aaca1910
dunglongtran <dunglongtran@gmail.com>
2018-07-10 00:21:31 +0700

    Add Sass
    Add Sass

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
 
-import App from './components/App'
-import './index.css'
+import App from "./components/App";
+import "./index.scss";
 
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from "./registerServiceWorker";
 
+//theme:http://highseastudio.com/demo/asana-wp/
 // Replace this with your project's endpoint
-const GRAPHCMS_API = 'https://api.graphcms.com/simple/v1/starterBlog'
+const GRAPHCMS_API = "https://api.graphcms.com/simple/v1/starterBlog";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: GRAPHCMS_API }),
   cache: new InMemoryCache()
-})
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
-)
-registerServiceWorker()
+  document.getElementById("root")
+);
+registerServiceWorker();
